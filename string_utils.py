6f72ac6af5e355eb0684b71112bc0ec36784f11b
323848200 <hadarfarkas@gmail.com>
2025-11-19 18:53:22 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -20,5 +20,6 @@ def split_before_each_uppercases(formula):
 def split_at_first_digit(formula):
     for char_index, char in enumerate(formula):
         if char.isdigit():
-            return formula[:char_index], int(formula[char_index:])
+            return formula[:char_index],
+int(formula[char_index:])
     return formula, 1
