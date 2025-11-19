93173256b1fe8ab8758273e38f6e39c6f6f7129c
323848200 <hadarfarkas@gmail.com>
2025-11-19 18:46:26 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,5 +1,4 @@
-def split_at_first_digit(formula):
-    def split_before_each_uppercases(formula):
+def split_before_each_uppercases(formula):
     start = 0
     end = 1
     elements_lst = []
@@ -22,4 +21,4 @@ def split_at_first_digit(formula):
     for char_index, char in enumerate(formula):
         if char.isdigit():
             return formula[:char_index], int(formula[char_index:])
-    return formula, 1 
+    return formula, 1
