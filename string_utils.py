1639e9c35aef84dc91ecced71c108de2e9c83f5c
323848200 <hadarfarkas@gmail.com>
2025-11-19 18:37:48 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,7 +1,3 @@
-def split_before_each_uppercases(formula):
-    pass # Replace the `pass` with your code
-
-
 def split_at_first_digit(formula):
     def split_before_each_uppercases(formula):
     start = 0
