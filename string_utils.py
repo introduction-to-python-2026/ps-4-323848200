9bf0f41139471a281ea06121b87ba4c5b29be27c
323848200 <hadarfarkas@gmail.com>
2026-01-23 13:16:52 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -2,21 +2,22 @@ def split_before_each_uppercase(formula):
     if not formula:
         return []
     
-    split_formula = []
-    start = 0
-    for end in range(1, len(formula)):
-        if formula[end].isupper():
-            split_formula.append(formula[start:end])
-            start = end
-            
-    split_formula.append(formula[start:])
-    return split_formula
+    indices = [i for i, char in enumerate(formula) if char.isupper()]
+    
+    indices.append(len(formula))
+    
+    result = []
+    for j in range(len(indices) - 1):
+        result.append(formula[indices[j]:indices[j+1]])
+        
+    return result
 
 
 def split_at_digit(formula):
     for i, char in enumerate(formula):
         if char.isdigit():
             prefix = formula[:i]
-            return prefix, int(formula[i:])
+            number = int(formula[i:])
+            return prefix, number
             
     return formula, 1
